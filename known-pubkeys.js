a4aa32fce3a9c57596cc8c7dce806d319ce304c3
inuitwallet <contact@inuit-wallet.co.uk>
2017-12-26 21:32:01 +0000

    adjust known pubkeys file to show ne wNu identities

--- known-pubkeys.js
+++ known-pubkeys.js
@@ -1,152 +1,98 @@
 (function () {
 	var memberOf = {
 		Nu: {
-			FLOT_NBT: 1,
-			FLOT_NSR: 2,
-			FLOT_BTC: 3
+			SMG_USNBT: 1,
+			SMG_USNBT_2_OF_3: 2
 		}
 	}
-	
+
 	var known = {
 		pubKey: {
 			// "pubkey": {name:"John Doe"},
-			
+
 			// Developer
 			"02113443efda4a9fe9bd38ca1f932aaee2c3cb6ee637f22eaa25af370a1cde6952": {name:"Developer", email:"ttutdxh.nubits@gmail.com", fee:1},
 		},
 		scriptHash: {
-			// Nu FLOT addresses scripthash. // Commented with redeemScript.
-			"f716edbaa472c1470d36a3e78b17a4c9bb547fcf": {name:"Nu FLOT BTC group 1st address", deprecated:true},           // 55210234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c072102435b894b94b4b27dd24436b3f9ad0b9409d855ab4be6e91141d445804e84750b2102e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f210312cd6eb361c9ebb0d90e44946492a237eab4c7a7d88a0db800f2f460937cc22f21034b0bd0f653d4ac0a2e9e81eb1863bb8e5743f6cb1ea40d845b939c225a1a80ff2103661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc2103a5fc09a5de595e4758bfbc3a932e2c448cc49a557dd6e64788cee530a76225212103da8082062298c40f0b473b74f3c95b57eaaebe3e67ed30ce56347b2e727915fb58ae
-			"afd5c3c6fb0c40799f0a874396df7bfce2bc29a3": {name:"Nu FLOT BTC group 2nd address"},                            // 55210234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c072102435b894b94b4b27dd24436b3f9ad0b9409d855ab4be6e91141d445804e84750b2102e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f210312cd6eb361c9ebb0d90e44946492a237eab4c7a7d88a0db800f2f460937cc22f210326c19862fb329470c828ace6749b64c50b4d9a8da60d60e4e32ebe96b388b2ae21034b0bd0f653d4ac0a2e9e81eb1863bb8e5743f6cb1ea40d845b939c225a1a80ff2103661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc2103da8082062298c40f0b473b74f3c95b57eaaebe3e67ed30ce56347b2e727915fb58ae
+			// Bis6BdPwdRg5dQiCEKAhjCwDkxpsGzKeJ6
+			"a55f3c8f377f2b9436ce3c42d12bff7b6b9d5f76": {name: "SMG USNBT 2 OF 3"},
+			// 522103c36f60a916fc9480983bc5166949dea09a4815592271160118ee965f2f339b17210396f3d2bdb56bf460a14532fcc523090a239ab1c8de4a083d55111a59dca7a3f32103af7d10aed63a3d91cbacdd9f68b2e197e0a9696b54210278bbc061f5ba6eb92653ae
 
-			"f35b24f264597d66ba8c366a5005824bb6a06138": {name:"Nu FLOT NBT group 1st address"},                            // 5321034b0bd0f653d4ac0a2e9e81eb1863bb8e5743f6cb1ea40d845b939c225a1a80ff2102a144af74d018501f03d76ead130433335f969772792ec39ce389c8a2341552592103661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc210234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c072102547427fc2ea3a0ab9ef70f5e1640ff5112b113f65696948f992bd0770b94257155ae
-			
-			"d4f1e97d23cf339c35b509de707acdcb9886e03b": {name:"Nu FLOT NSR group provisional address", deprecated:true},     // 53210234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c072102a144af74d018501f03d76ead130433335f969772792ec39ce389c8a2341552592103661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc2103686ee42f635c71c08f326e66139b6cb37167402cc0562584655aac03fe74049521039854d0e2abf6e4971e1350137b876da6a05132737c11ca3e37aaed2a0eb6680855ae
-			"7077487df91c0166d33ee6079b0c2229788f6ac2": {name:"Nu FLOT NSR group 1st address"},                            // 532102e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f2103661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc2103686ee42f635c71c08f326e66139b6cb37167402cc0562584655aac03fe74049521039854d0e2abf6e4971e1350137b876da6a05132737c11ca3e37aaed2a0eb668082103d05d8fb69fcd289548140dde8e906f6fc217b02477f81842f2483d9ea1c9242555ae
+			// BpDJpPxgHbEkNmeivsLu4RqBPCaBsjsqCB
+			"e00abb2275d64badd6ba14d7090e6fc647749199": {name: "SMG USNBT"},
+			// 522102b40eac372686090d563c5535b94137d02d986b99c318f5de5140b13f2d12b1e621025644ea515e6fac176efa63fe19e0342b19b3f32f27583e3d0a1bb78c966f28b321033eedb31d02dc810cad13c3c0c51120967e9ae2cfae6fa803f73bd6117f0691f8210312d4ebbf0c5922d21099a92e508787af887beff4a4d1be1deb1dacb0889d5f9254ae
 		},
 		identities: { // id.pubkey{memberof, [deprecated]}
-			"cryptog": {
-				"02435b894b94b4b27dd24436b3f9ad0b9409d855ab4be6e91141d445804e84750b": {
+			"woolly_sammoth": {
+				"02b40eac372686090d563c5535b94137d02d986b99c318f5de5140b13f2d12b1e6": {
 					member: [
-						memberOf.Nu.FLOT_BTC  // https://discuss.nubits.com/t/via-quote/3098/15
+						memberOf.Nu.SMG_USNBT
 					]
 				},
-				"03686ee42f635c71c08f326e66139b6cb37167402cc0562584655aac03fe740495": {
+				"03c36f60a916fc9480983bc5166949dea09a4815592271160118ee965f2f339b17": {
 					member: [
-						memberOf.Nu.FLOT_NSR  // https://discuss.nubits.com/t/via-quote/3103/20
-					]
-				},
-			},
-			"Dhume": {
-				"02e2fcdfe246e9cd4864d9119b8af465487385eccd0ea30a8cb21d44d36818189f": {
-					member: [
-						memberOf.Nu.FLOT_BTC, // https://discuss.nubits.com/t/via-quote/3098/10
-						memberOf.Nu.FLOT_NSR  // https://discuss.nubits.com/t/via-quote/3103/14
+						memberOf.Nu.SMG_USNBT_2_OF_3
 					]
 				}
 			},
-			"dysconnect": {
-				"034b0bd0f653d4ac0a2e9e81eb1863bb8e5743f6cb1ea40d845b939c225a1a80ff": {
+			"Cybnate": {
+				"033eedb31d02dc810cad13c3c0c51120967e9ae2cfae6fa803f73bd6117f0691f8": {
 					member: [
-						memberOf.Nu.FLOT_BTC, // https://discuss.nubits.com/t/via-quote/3098/16
-						memberOf.Nu.FLOT_NBT  // https://discuss.nubits.com/t/via-quote/2902/167
+						memberOf.Nu.SMG_USNBT
 					]
 				}
 			},
 			"jooize": {
-				"0234139729dd413c84a71a0bfd6f236790be861b37311cef3240277c940e4b0c07": {
+				"025644ea515e6fac176efa63fe19e0342b19b3f32f27583e3d0a1bb78c966f28b3": {
 					member: [
-						memberOf.Nu.FLOT_BTC, // https://discuss.nubits.com/t/via-quote/3098/13
-						memberOf.Nu.FLOT_NBT  // https://discuss.nubits.com/t/via-quote/2902/170
+						memberOf.Nu.SMG_USNBT
+					]
+				},
+				"0396f3d2bdb56bf460a14532fcc523090a239ab1c8de4a083d55111a59dca7a3f3": {
+					member: [
+						memberOf.Nu.SMG_USNBT_2_OF_3
 					]
 				}
 			},
-			"masterOfDisaster": {
-				"0312cd6eb361c9ebb0d90e44946492a237eab4c7a7d88a0db800f2f460937cc22f": {
+			"Phoenix": {
+				"0312d4ebbf0c5922d21099a92e508787af887beff4a4d1be1deb1dacb0889d5f92": {
 					member: [
-						memberOf.Nu.FLOT_BTC  // https://discuss.nubits.com/t/via-quote/3098/12
+						memberOf.Nu.SMG_USNBT
 					]
 				},
-				"02a144af74d018501f03d76ead130433335f969772792ec39ce389c8a234155259": {
+				"03af7d10aed63a3d91cbacdd9f68b2e197e0a9696b54210278bbc061f5ba6eb926": {
 					member: [
-						memberOf.Nu.FLOT_NBT  // https://discuss.nubits.com/t/via-quote/2902/168
-					]
-				},
-				"03d05d8fb69fcd289548140dde8e906f6fc217b02477f81842f2483d9ea1c92425": {
-					member: [
-						memberOf.Nu.FLOT_NSR  // https://discuss.nubits.com/t/via-quote/3103/1
-					]
-				}
-			},
-			"mhps": {
-				"03a5fc09a5de595e4758bfbc3a932e2c448cc49a557dd6e64788cee530a7622521": {
-				deprecated: true, // https://discuss.nubits.com/t/flot-operations-buy-side-btc-related/3117/168
-				member: [
-						memberOf.Nu.FLOT_BTC  // https://discuss.nubits.com/t/via-quote/3098/14
-					]
-				},
-				"0326c19862fb329470c828ace6749b64c50b4d9a8da60d60e4e32ebe96b388b2ae": {
-					member: [
-						memberOf.Nu.FLOT_BTC  // https://discuss.nubits.com/t/via-quote/3098/14
-					]
-				},
-				"039854d0e2abf6e4971e1350137b876da6a05132737c11ca3e37aaed2a0eb66808": {
-					member: [
-						memberOf.Nu.FLOT_NSR  // https://discuss.nubits.com/t/via-quote/3103/9
-					]
-				}
-			},
-			"ttutdxh": {
-				"03661a4370dfcfbcea25d1800057220f4572b6eecab95bb0670e8676a9e34451dc": {
-					member: [
-						memberOf.Nu.FLOT_BTC, // https://discuss.nubits.com/t/via-quote/3098/9
-						memberOf.Nu.FLOT_NBT, // https://discuss.nubits.com/t/via-quote/2902/169
-						memberOf.Nu.FLOT_NSR  // https://discuss.nubits.com/t/via-quote/3103/6
-					]
-				}
-			},
-			"woodstockmerkle": {
-				"03da8082062298c40f0b473b74f3c95b57eaaebe3e67ed30ce56347b2e727915fb": {
-					member: [
-						memberOf.Nu.FLOT_BTC  // https://discuss.nubits.com/t/via-quote/3098/11
-					]
-				},
-				"02547427fc2ea3a0ab9ef70f5e1640ff5112b113f65696948f992bd0770b942571": {
-					member: [
-						memberOf.Nu.FLOT_NBT  // https://discuss.nubits.com/t/via-quote/2902/171
+						memberOf.Nu.SMG_USNBT_2_OF_3
 					]
 				}
 			}
 		}
 	};
-	
+
 
 	for (var id in known.identities) { // Compute pubkey list
 		for (var pubkey in known.identities[id]) {
-			
-			// FLOT memberOfs
-			var FLOT = [];
+
+			// Nu Groups
+			var NU = [];
 			for (var group in known.identities[id][pubkey].member) {
-				if (known.identities[id][pubkey].member[group] == memberOf.Nu.FLOT_BTC) FLOT.push("BTC");
-				if (known.identities[id][pubkey].member[group] == memberOf.Nu.FLOT_NBT) FLOT.push("NBT");
-				if (known.identities[id][pubkey].member[group] == memberOf.Nu.FLOT_NSR) FLOT.push("NSR");
+				if (known.identities[id][pubkey].member[group] == memberOf.Nu.SMG_USNBT) NU.push("SMG_USNBT");
+				if (known.identities[id][pubkey].member[group] == memberOf.Nu.SMG_USNBT_2_OF_3) NU.push("SMG_USNBT_2_OF_3");
 			}
-			if (FLOT.length > 0) {
+			if (NU.length > 0) {
 				var prefix = (known.identities[id][pubkey].deprecated)?"DEPRECATED ":"";
 				known.pubKey[pubkey] = {
-					name: prefix + "Nu FLOT @"+ id + " " +(FLOT.join(", ")),
-					email: "<contact directly>",
-					fee: ""
+					name: prefix + " " + id + " [ " + (NU.join(", ")) + " ]",
 				}
 			}
 		}
 	}
-	
+
 	// Mark deprecated. Handling will change in the future. Maybe an alert.
 	for (var hash in known.scriptHash) {
 		known.scriptHash[hash].name = (known.scriptHash[hash].deprecated)?"DEPRECATED " + known.scriptHash[hash].name:known.scriptHash[hash].name;
 	}
-	
+
 	window['known'] = known;
 })();
 
